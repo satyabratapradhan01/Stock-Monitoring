9899c3d232ae94624898a9c5722d8d424945f454
satyabratapradhan <satyabratapradhann@gmail.com>
2025-06-21 00:19:21 +0530

    create sever for backend then create schema and model. moongoAtlas store the data

---
+++ backend/model/PositionsModel.js
@@ -0,0 +1,5 @@
+
+const {model} = require("mongoose");
+const { PositionsSchema } = require("../schemas/PositionSchema");
+const PositionsModel = new model("position", PositionsSchema);
+module.exports = { PositionsModel };
